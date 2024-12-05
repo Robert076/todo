a99a61d55ff81df28d6f03906d10201542915990
Robert076 <robertberes06@gmail.com>
2024-12-05 18:03:27 +0200

    feat: read

--- main/main.go
+++ main/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
+)
 
+type todo struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+var todos = []todo{
+	{ID: "1", Title: "Fill up on gas", Description: "We will be going on a roadtrip and there is no gas in the car"},
+	{ID: "2", Title: "Do the Java homework", Description: "In a few days we will have the final exam in Java. We must be prepared"},
+	{ID: "3", Title: "Cook a healthy meal", Description: "We must get all the protein our body needs, so cook something healthy"},
+}
+
+func myfunc() {
+
+}
+
+func main() {
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todos)
+	})
+	http.ListenAndServe(":8080", nil)
 }
