8923cdfe1717934276edfc18ae684418b2cc2146
Robert076 <robertberes06@gmail.com>
2024-12-06 10:03:00 +0200

    feat: delete by id implementation

--- main/main.go
+++ main/main.go
@@ -17,6 +17,8 @@ var todos = []todo{
 	{ID: "1", Title: "Fill up on gas", Description: "We will be going on a roadtrip and there is no gas in the car"},
 	{ID: "2", Title: "Do the Java homework", Description: "In a few days we will have the final exam in Java. We must be prepared"},
 	{ID: "3", Title: "Cook a healthy meal", Description: "We must get all the protein our body needs, so cook something healthy"},
+	{ID: "4", Title: "Write a RESTful API in GO", Description: "Go is a fun language so we must learn it"},
+	{ID: "5", Title: "Clean the house", Description: "We will have guests coming over so let's clean the house"},
 }
 
 func main() {
@@ -33,6 +35,17 @@ func main() {
 			json.NewEncoder(w).Encode(nil)
 		} else {
 			json.NewEncoder(w).Encode(todos[idInt])
+		}
+	})
+	http.HandleFunc("/todos/delete", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+		idInt, err := strconv.Atoi(id)
+
+		if len(todos) <= idInt || idInt < 0 || err != nil {
+			json.NewEncoder(w).Encode(nil)
+		} else {
+			json.NewEncoder(w).Encode("Deleting id " + id)
+			todos = append(todos[:idInt], todos[idInt+1:]...)
 		}
 	})
 	http.ListenAndServe(":8080", nil)
